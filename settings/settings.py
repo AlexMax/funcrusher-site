5677732e10e4e5347b9a1e4ee9874b2b19ec7a41
Alex Mayfield <alexmax2742@gmail.com>
2011-10-08 14:17:13 -0400

    Base template and registration form.

--- settings/settings.py
+++ settings/settings.py
@@ -55,9 +55,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    PROJECT_ROOT + 'templates/',
 )
 
 INSTALLED_APPS = (
